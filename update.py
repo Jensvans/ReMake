51e9e1e7d04a738046a7ab503e33effc518bc8e2
JensV <jens.vansteenvoort@gmail.com>
2021-08-18 18:05:44 +0200

    test2

--- update.py
+++ update.py
@@ -27,8 +27,8 @@ def main():
     Execute("sudo apt-get upgrade")
 
   if number == 2:
-    prgrm = input("name of program: ")
-    Execute("sudo apt-get install"+ prgrm())
+
+    Execute("sudo apt-get install"+ input("name of progrom: "))
 
 if __name__ == '__main__':
     main()
